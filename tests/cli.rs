0181b7cc61d29dfe340f2a68f40b028231aea7dc
kenken <kengoviolinphoto@gmail.com>
2024-07-28 17:24:10 +0900

    Refactor test code

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use assert_cmd;
+    use assert_cmd::Command;
     use predicates::prelude::*;
     use std::fs;
 
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn test_no_args() -> TestResult {
-        let mut cmd = assert_cmd::Command::cargo_bin("secho")?;
+        let mut cmd = Command::cargo_bin("secho")?;
         cmd.assert()
             .failure()
             .stderr("error: the following required arguments were not provided:\n  <TEXT>...\n\nUsage: secho <TEXT>...\n\nFor more information, try '--help'.\n");
@@ -17,7 +17,7 @@ mod tests {
 
     #[test]
     fn test_run() -> TestResult {
-        let mut cmd = assert_cmd::Command::cargo_bin("secho")?;
+        let mut cmd = Command::cargo_bin("secho")?;
         cmd.arg("hello").assert().success();
         Ok(())
     }
@@ -26,7 +26,7 @@ mod tests {
     fn test_hello1() -> TestResult {
         let outfile = "tests/expected/hello1.txt";
         let expected_value = fs::read_to_string(outfile)?;
-        let mut cmd = assert_cmd::Command::cargo_bin("secho")?;
+        let mut cmd = Command::cargo_bin("secho")?;
         cmd.arg("Hello there")
             .assert()
             .success()
