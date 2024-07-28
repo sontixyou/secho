2556d2824197317819cce020920ba8012ab56201
kenken <kengoviolinphoto@gmail.com>
2024-07-28 16:07:46 +0900

    Add failure message

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,6 +5,8 @@ mod tests {
     #[test]
     fn test_no_args() {
         let mut cmd = assert_cmd::Command::cargo_bin("secho").unwrap();
-        cmd.assert().failure();
+        cmd.assert()
+            .failure()
+            .stderr("error: the following required arguments were not provided:\n  <TEXT>...\n\nUsage: secho <TEXT>...\n\nFor more information, try '--help'.\n");
     }
 }
