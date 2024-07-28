6f384be3800df443a64eeacbde34519be5a95bf5
kenken <kengoviolinphoto@gmail.com>
2024-07-28 16:11:59 +0900

    Add test of success

--- tests/cli.rs
+++ tests/cli.rs
@@ -9,4 +9,10 @@ mod tests {
             .failure()
             .stderr("error: the following required arguments were not provided:\n  <TEXT>...\n\nUsage: secho <TEXT>...\n\nFor more information, try '--help'.\n");
     }
+
+    #[test]
+    fn test_run() {
+        let mut cmd = assert_cmd::Command::cargo_bin("secho").unwrap();
+        cmd.arg("hello").assert().success();
+    }
 }
